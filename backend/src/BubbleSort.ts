1b42774d7dae0f8df18907db090cd4c729a695ea
stiggu <abelmunda@hotmail.com>
2022-04-27 09:41:59 -0500

    Keeping convention, placed the bubble sort function in it's own file

---
+++ backend/src/BubbleSort.ts
@@ -0,0 +1,19 @@
+﻿export default function bubbleSort(inputs: number[]): number[][] {
+    const matrix: number[][] = [];
+
+    for(let x = 0; x < inputs.length; x++){
+        for(let y = 0; y < inputs.length; y++){
+            if(y == inputs.length - 1){
+                continue;
+            }
+            if(inputs[y] > inputs[y + 1]){
+                const fix:number = inputs[y];
+                inputs[y] = inputs[y + 1];
+                inputs[y + 1] = fix;
+            }
+            matrix.push(inputs.slice());
+        }
+    }
+    return matrix;
+}
+    
--- backend/src/sorter.ts
+++ backend/src/sorter.ts
@@ -1,22 +1,8 @@
 import Quicksort from "./Quicksort";
+import bubbleSort from "./BubbleSort";
 
 export function bubble(inputs: number[]): number[][] {
-    const matrix: number[][] = [];
-
-    for(let x = 0; x < inputs.length; x++){
-        for(let y = 0; y < inputs.length; y++){
-            if(y == inputs.length - 1){
-                continue;
-            }
-            if(inputs[y] > inputs[y + 1]){
-                const fix:number = inputs[y];
-                inputs[y] = inputs[y + 1];
-                inputs[y + 1] = fix;
-            }
-            matrix.push(inputs.slice());
-        }
-    }
-    return matrix;
+    return bubbleSort(inputs);
 }
 
 export function quick(input: number[]): number[][] {
